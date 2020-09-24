9691cdd1f59a29ce8dd65db4b60e26883ae951dc
Se min Lee <mlsml94@gmail.com>
2020-09-24 10:39:59 -0400

    departments

--- mycart/src/App.js
+++ mycart/src/App.js
@@ -1,60 +1,15 @@
 import React, { Component } from 'react';
 import './App.css';
-import {
-  Switch,
-  Route,
-  Link
-} from "react-router-dom";
-import Departments from "./containers/Departments.js";
-import MyRecipes from "./containers/MyRecipes.js";
-import Cart from "./containers/Cart.js";
-require('dotenv').config()
+import Header from "./components/Header.js";
 
-// console.log(process.env)
 class App extends Component {
-  
-  state = {
-    recipeArray: []
-  }
-
-  componentDidMount(){
-    fetch(`https://api.spoonacular.com/recipes/findByNutrients?minCarbs=10&maxCarbs=50&number=2&apiKey=${process.env.REACT_APP_SPOONACULAR_KEY}`)
-      .then(resp=>resp.json())
-      .then(data=>this.setState(()=>({
-        recipeArray: data })))
-  }
-
 
   render() {
-    console.log(this.state.recipeArray)
     return(
-      <div>
-        <ul>
-          <li>
-            <Link to="/departments">Departments</Link>
-          </li>
-          <li>
-            <Link to="/cart">Cart</Link>
-          </li>
-          <li>
-            <Link to="/myrecipes">myrecipes</Link>
-          </li>
-        </ul>
-
-        <hr />
-
-        <Switch>
-          <Route path="/departments">
-            <Departments />
-          </Route>
-          <Route path="/cart">
-            <Cart />
-          </Route>
-          <Route path="/myrecipes">
-            <MyRecipes recipe={this.state.recipeArray} />
-          </Route>
-        </Switch>
-      </div>
+      <>
+        <Header />
+        
+      </>
     )
   }
 }
--- mycart/src/components/Recipe.js
+++ mycart/src/components/Recipe.js
@@ -3,7 +3,7 @@ import React from 'react'
 const Recipe = (props) => {
 return (
 
-        <div>
+        <div className="recipe-card">
             <h2>{props.recipe.title}</h2>
             <img src={props.recipe.image} alt={props.recipe.title}/>
         </div>
