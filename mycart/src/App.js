6afd5e0cab719e1b46ea327edcc42bb12cb06423
Se min Lee <mlsml94@gmail.com>
2020-09-24 18:01:36 -0400

    commit

--- mycart/src/App.js
+++ mycart/src/App.js
@@ -7,43 +7,43 @@ import Navbar from "./components/Navbar/Navbar.js";
 
 class App extends Component {
 
-  // state = {
-  //   itemArray: [],
-  //   searchValue: "",
-  // }
+  state = {
+    itemArray: [],
+    searchValue: "",
+  }
 
-  // componentDidMount() {
-  //     fetch("http://localhost:8000/items")
-  //     .then((res) => res.json())
-  //     .then((data) => this.setState({ itemArray: data }));
-  // }
+  componentDidMount() {
+      fetch("http://localhost:8000/items")
+      .then((res) => res.json())
+      .then((data) => this.setState({ itemArray: data }));
+  }
  
 
-  // search = (searchValue) => {
-  //   this.setState({ searchValue: searchValue });
-  //   console.log("settingSearch:", this.state.searchValue)
-  // };
+  search = (searchValue) => {
+    this.setState({ searchValue: searchValue });
+    console.log("settingSearch:", this.state.searchValue)
+  };
 
-  // showItemArray = () => {
-  // return this.state.searchValue === ""
-  //   ? this.state.itemArray
-  //   : this.state.itemArray.filter(
-  //       (item) =>
-  //         item.name
-  //           .toLowerCase()
-  //           .includes(this.state.searchValue.toLowerCase()) ||
-  //         item.description.toLowerCase().includes(this.state.searchValue.toLowerCase())
-  //       )
-  // }
+  showItemArray = () => {
+  return this.state.searchValue === ""
+    ? this.state.itemArray
+    : this.state.itemArray.filter(
+        (item) =>
+          item.name
+            .toLowerCase()
+            .includes(this.state.searchValue.toLowerCase()) ||
+          item.description.toLowerCase().includes(this.state.searchValue.toLowerCase())
+        )
+  }
 
   render() {
 
   return(     
     <div className="App">
       <Navbar />
-      {/* <Search className="searchBar" searchValue={this.state.searchValue} search={this.search}/>
+      <Search className="searchBar" searchValue={this.state.searchValue} search={this.search}/>
       
-      <TempIndex className="temporary-search-index" itemArray={this.showItemArray}/> */}
+      <TempIndex className="temporary-search-index" itemArray={this.showItemArray()}/>
 
     </div>
         
