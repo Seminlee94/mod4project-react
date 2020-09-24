86453e63f4c92059d61ca2116b1624c2c6e51bb2
kevin xie <xie.kevin.j@gmail.com>
2020-09-23 22:18:51 -0400

    cleaned up file

--- my-project-client/src/App.js
+++ my-project-client/src/App.js
@@ -7,18 +7,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
          <Header />
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
