41524809eda144fd0405255c8c63f9165c810367
kevin_xie <xie.kevin.j@gmail.com>
2020-09-25 14:50:24 -0400

    broken shelves

--- mycart/src/App.js
+++ mycart/src/App.js
@@ -1,7 +1,8 @@
 import React, { Component } from "react";
 import "./App.css";
-import {BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
-// import HomeIndex from "./containers/HomeIndex";
+import { Switch, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router } from "react-router-dom";
+import HomeIndex from "./containers/HomeIndex";
 import Navbar from "./components/Navbar/Navbar.js";
 import Search from "./components/Home/Search.js";
 import TempIndex from "./containers/TempIndex.js";
@@ -10,103 +11,109 @@ import Friends from "./containers/Friends.js";
 import Shop from "./containers/Shop.js";
 
 class App extends Component {
-
   state = {
     itemArray: [],
     searchValue: "",
-    fridgeItemArray: []
-    // displayIndex: 0
-  }
+    fridgeItemArray: [],
+  };
 
   componentDidMount() {
     fetch("http://localhost:8000/items")
-    .then((res) => res.json())
-    .then((data) => this.setState({ itemArray: data }));
+      .then((res) => res.json())
+      .then((data) => this.setState({ itemArray: data }));
   }
 
   componentWillMount() {
     fetch("http://localhost:8000/fridge-items")
-      .then(resp => resp.json())
-      .then(data => this.setState({ fridgeItemArray: data[0].items }))
+      .then((resp) => resp.json())
+      .then((data) => this.setState({ fridgeItemArray: data[0].items }));
   }
- 
+
   // renderFourItems = () => {
   //   return this.state.fridgeItemArray.slice(this.state.displayIndex, this.state.displayIndex+4)
   // }
 
-  more = (event) => {
-    let newDisplayIndex = this.state.displayIndex + 4
+  // more = (event) => {
+  //   let newDisplayIndex = this.state.displayIndex + 4
 
-    //bonus
-    if(newDisplayIndex >= this.state.fridgeItemArray.length){
-      newDisplayIndex = 0
-    }
+  //   //bonus
+  //   if(newDisplayIndex >= this.state.fridgeItemArray.length){
+  //     newDisplayIndex = 0
+  //   }
 
-    this.setState({
-      displayIndex: newDisplayIndex
-    })
-  }
+  //   this.setState({
+  //     displayIndex: newDisplayIndex
+  //   })
+  // }
 
   search = (searchValue) => {
     this.setState({ searchValue: searchValue });
-    console.log("settingSearch:", this.state.searchValue)
+    console.log("settingSearch:", this.state.searchValue);
   };
 
   showItemArray = () => {
-  return this.state.searchValue === ""
-    ? this.state.itemArray
-    : this.state.itemArray.filter(
-        (item) =>
-          item.name
-            .toLowerCase()
-            .includes(this.state.searchValue.toLowerCase()) ||
-          item.description.toLowerCase().includes(this.state.searchValue.toLowerCase())
-        )
-  }
+    return this.state.searchValue === ""
+      ? this.state.itemArray
+      : this.state.itemArray.filter(
+          (item) =>
+            item.name
+              .toLowerCase()
+              .includes(this.state.searchValue.toLowerCase()) ||
+            item.description
+              .toLowerCase()
+              .includes(this.state.searchValue.toLowerCase())
+        );
+  };
 
   render() {
+    return (
+      <div
+        className="App"
+        style={{
+          height: "100%",
+          position: "absolute",
+          left: "0px",
+          width: "100%",
+          overflow: "hidden",
+        }}
+      >
+        <Router>
+          <Navbar />
 
-  return( 
-    <div className="App" style={{ height: '100%', position: 'absolute', left: '0px', width: '100%', overflow: 'hidden'}}>
-      <Router>
-
-        <Navbar />
-        
-        <Switch class="header-switch">
-
-          <Route path="/shop">
+          <Switch class="header-switch">
+            <Route path="/shop">
               <Shop />
-          </Route>
+            </Route>
 
-          <Route path="/fridge">
+            <Route path="/fridge">
               <Fridge item={this.state.fridgeItemArray} />
-              {/* <Fridge item={this.renderFourItems()} more={this.more} /> */}
-          </Route>
+            </Route>
 
-          <Route path="/friends">
+            <Route path="/friends">
               <Friends />
-          </Route>
+            </Route>
 
-          <Route path="/">
-            <Search className="searchBar" searchValue={this.state.searchValue} search={this.search}/> 
-            <TempIndex className="temporary-search-index" itemArray={this.showItemArray()}/>
-          </Route>
-
-        </Switch>
-      </Router>
-
-
-    </div>
-        
-
-  )
-  // render() {
-  //   return (
-  //     <div className="app">
-  //       <Navbar />
-  //       <HomeIndex />
-  //     </div>
-  //   );
+            <Route path=""></Route>
+          </Switch>
+        </Router>
+        <Search
+          className="searchBar"
+          searchValue={this.state.searchValue}
+          search={this.search}
+        />
+        <HomeIndex
+          className="temporary-search-index"
+          itemArray={this.showItemArray()}
+        />
+      </div>
+    );
+    // render() {
+    //   return (
+    //     <div className="app">
+    //       <Navbar />
+    //       <HomeIndex />
+    //     </div>
+    //   );
   }
 }
 
--- mycart/src/containers/Fridge.js
+++ mycart/src/containers/Fridge.js
@@ -1,114 +1,152 @@
-import React from 'react'
+import React from "react";
 import FridgeItem from "../components/Fridge/FridgeItem.js";
 import "../components/Fridge/Fridge.css";
 // import MoreButton from "../components/Home/MoreButton.js";
+import Shelf from "../components/Shelf";
 
 class Fridge extends React.Component {
-
   // state = {
-  //   displayIndex: 0
-  // }
-
   // items = () => {
   //   return this.props.item.map(item => <FridgeItem item={item} /> )
   // }
+  //   displayIndex: 0,
+  // };
+
+  // more = (event) => {
+  //   let newDisplayIndex = this.state.displayIndex + 4;
+  //   if (newDisplayIndex >= this.state.fridgeItemArray.length) {
+  //     newDisplayIndex = 0;
+  //   }
+
+  //   this.setState({
+  //     displayIndex: newDisplayIndex,
+  //   });
+  // };
 
   filterBakery = () => {
-    let filteredBakery = this.props.item.filter(item => item.category==="Bread")
-    return filteredBakery.map(el => <FridgeItem item={el} />) 
-  }
+    let filteredBakery = this.props.item.filter(
+      (item) => item.category === "Bread"
+    );
+    return filteredBakery.map((el) => <FridgeItem item={el} />);
+  };
 
   filterProduce = () => {
-    let filteredProduce = this.props.item.filter(item => item.category==="Produce")
-    return filteredProduce.map(el => <FridgeItem item={el} />) 
-  }
+    let filteredProduce = this.props.item.filter(
+      (item) => item.category === "Produce"
+    );
+    return filteredProduce.map((el) => <FridgeItem item={el} />);
+  };
 
   filterMeat = () => {
-    let filteredMeat = this.props.item.filter(item => item.category==="Meat")
-    return filteredMeat.map(el => <FridgeItem item={el} />) 
-  }
+    let filteredMeat = this.props.item.filter(
+      (item) => item.category === "Meat"
+    );
+    return filteredMeat.map((el) => <FridgeItem item={el} />);
+  };
 
   filterSeafood = () => {
-    let filteredSeafood = this.props.item.filter(item => item.category==="Seafood")
-    return filteredSeafood.map(el => <FridgeItem item={el} />) 
-  }
+    let filteredSeafood = this.props.item.filter(
+      (item) => item.category === "Seafood"
+    );
+    return filteredSeafood.map((el) => <FridgeItem item={el} />);
+  };
 
   filterLiquors = () => {
-    let filteredLiquors = this.props.item.filter(item => item.category==="Liquors")
-    return filteredLiquors.map(el => <FridgeItem item={el} />) 
-  }
+    let filteredLiquors = this.props.item.filter(
+      (item) => item.category === "Liquors"
+    );
+    return filteredLiquors.map((el) => <FridgeItem item={el} />);
+  };
 
   filterPreparedFood = () => {
-    let filteredPreparedFood = this.props.item.filter(item => item.category==="Prepared Food")
-    return filteredPreparedFood.map(el => <FridgeItem item={el} />) 
-  }
+    let filteredPreparedFood = this.props.item.filter(
+      (item) => item.category === "Prepared Food"
+    );
+    return filteredPreparedFood.map((el) => <FridgeItem item={el} />);
+  };
 
   filterOther = () => {
-    let filteredOther = this.props.item.filter(item => item.category==="Other Departments")
-    return filteredOther.map(el => <FridgeItem item={el} />) 
-  }
-  
-  filterCheese = () => {
-    let filteredCheese = this.props.item.filter(item => item.category==="Cheese")
-    return filteredCheese.map(el => <FridgeItem item={el} />) 
-  }
+    let filteredOther = this.props.item.filter(
+      (item) => item.category === "Other Departments"
+    );
+    return filteredOther.map((el) => <FridgeItem item={el} />);
+  };
 
+  filterCheese = () => {
+    let filteredCheese = this.props.item.filter(
+      (item) => item.category === "Cheese"
+    );
+    return filteredCheese.map((el) => <FridgeItem item={el} />);
+  };
+
+  filter = (Category) => {
+    let filteredBakery = this.props.item.filter(
+      (item) => item.category === Category
+    );
+    return filteredBakery.map((el) => <FridgeItem item={el} />);
+  };
   render() {
+    // const bakery
     return (
       <div className="fridge-container">
-
         <div className="fridge-container-left">
           <div className="fridge-shelf">
             <div className="fridge-category">Bakery</div>
-            <div className="fridge-item-card">{this.filterBakery()}
-            {/* <MoreButton moreItems={this.props.more} /> */}
-            </div> 
+            <div className="fridge-item-card">
+              <Shelf category={this.filterBakery()} />
+              <Shelf category={this.filterProduce()} />
+              <Shelf category={this.filterMeat()} />
+              <Shelf category={this.filterSeafood()} />
+              {/* <Shelf category={this.filterLiqours()} /> */}
+              {/* <Shelf category={this.filterPreparedFood()} />
+              <Shelf category={this.filterOther()} />
+              <Shelf category={this.filterCheese()} /> */}
+              {/* {this.filter("Bread")} */}
+            </div>
           </div>
 
           <div className="fridge-shelf">
             <div className="fridge-category">Produce</div>
-            <div className="fridge-item-card">{this.filterProduce()}</div> 
+            <div className="fridge-item-card">{this.filterProduce()}</div>
           </div>
 
           <div className="fridge-shelf">
             <div className="fridge-category">Meat</div>
-            <div className="fridge-item-card">{this.filterMeat()}</div> 
+            <div className="fridge-item-card">{this.filterMeat()}</div>
           </div>
 
           <div className="fridge-shelf">
             <div className="fridge-category">Seafood</div>
-            <div className="fridge-item-card">{this.filterSeafood()}</div> 
+            <div className="fridge-item-card">{this.filterSeafood()}</div>
           </div>
-
         </div>
 
         <div className="fridge-blank"></div>
 
         <div className="fridge-container-right">
           <div className="fridge-shelf">
-              <div className="fridge-category">Cheese</div>
-              <div className="fridge-item-card">{this.filterCheese()}</div> 
-            </div>
+            <div className="fridge-category">Cheese</div>
+            <div className="fridge-item-card">{this.filterCheese()}</div>
+          </div>
 
-            <div className="fridge-shelf">
-              <div className="fridge-category">Liquors</div>
-              <div className="fridge-item-card">{this.filterLiquors()}</div> 
-            </div>
+          <div className="fridge-shelf">
+            <div className="fridge-category">Liquors</div>
+            <div className="fridge-item-card">{this.filterLiquors()}</div>
+          </div>
 
-            <div className="fridge-shelf">
-              <div className="fridge-category">Prepared Food</div>
-              <div className="fridge-item-card">{this.filterPreparedFood()}</div> 
-            </div>
+          <div className="fridge-shelf">
+            <div className="fridge-category">Prepared Food</div>
+            <div className="fridge-item-card">{this.filterPreparedFood()}</div>
+          </div>
 
-            <div className="fridge-shelf">
-              <div className="fridge-category">Other</div>
-              <div className="fridge-item-card">{this.filterOther()}</div> 
-            </div>
+          <div className="fridge-shelf">
+            <div className="fridge-category">Other</div>
+            <div className="fridge-item-card">{this.filterOther()}</div>
+          </div>
         </div>
       </div>
-    )
+    );
   }
-
 }
 
-export default Fridge
+export default Fridge;
