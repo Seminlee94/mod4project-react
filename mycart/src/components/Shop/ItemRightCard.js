e18d9be64f623b4bd39500b0b16ca50743b1cd05
Se min Lee <mlsml94@gmail.com>
2020-09-27 15:55:23 -0400

    shoprightside

--- mycart/src/components/Shop/ItemRightCard.js
+++ mycart/src/components/Shop/ItemRightCard.js
@@ -4,18 +4,20 @@ import FridgeButton from "../Home/FridgeButton";
 const ItemRightCard = (props) => {
   return (
     <div className="item-right-card">
-      {/* <img src={props.item.image}/> */}
-      <strong>
-        <p>${props.item.price}</p>
-      </strong>
-      <p>{props.item.name}</p>
+      <img 
+        src={props.item.image}
+        style={{ width:"50px", height:"50px" }}  />
+      <div 
+        className="item-right-price">
+        ${props.item.price}
+      </div>
       <FridgeButton
         moveToFridge={props.moveToFridge}
         item={props.item.id}
         clickedItemIndex={props.clickedItemIndex}
-      >
-        Move To Fridge
-      </FridgeButton>
+      /> 
+
+
     </div>
   );
 };
--- mycart/src/components/Shop/Search.js
+++ mycart/src/components/Shop/Search.js
@@ -1,28 +1,3 @@
-// import React from 'react'
-
-// class Search extends React.Component {
-
-//     onChange = (e) => {
-//       this.props.search(e.target.value);
-//     };
-    
-//     render() {
-    
-//       return (
-//         <form className="search">
-//              <input
-//                 placeholder="search"
-//                 value={this.props.searchValue}
-//                 onChange={this.onChange}
-//              ></input>
-//              <button>Submit</button>
-//         </form>
-//       );
-//     }
-// }
-
-// export default Search
-
 import React from 'react'
 
 class Search extends React.Component {
@@ -49,7 +24,7 @@ class Search extends React.Component {
         <form className="search" onSubmit={this.submitHandler}>
              <input
                 type="text"
-                placeholder="search"
+                placeholder="Search Item"
                 value={this.state.searchValue}
                 onChange={this.onChange}
              ></input>
